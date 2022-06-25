a5a146d4ab4fb44422b0bac32ab39d6f1f098891
gunoooo <rjsdnqkr1@naver.com>
2022-06-25 22:23:49 +0900

    ✨ 프로젝트 세팅

---
+++ Workspace.swift
@@ -0,0 +1,12 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let workspace = Workspace(
+    name: "Marryting",
+    projects: [
+        "Projects/Marryting",
+        "Projects/Core",
+        "Projects/DesignSystem",
+        "Projects/Features/FeatureKit",
+    ]
+)
