5afbc807d6e42e92ae9398091a0b09b29e9b3d81
박건우 <39177603+gunoooo@users.noreply.github.com>
2022-06-28 18:25:07 +0900

    ✨ 프로젝트 세팅 (#10)
    * ✨ 프로젝트 세팅
    
    * ✨ 더미 파일 추가
    
    * 🔥 xcworkspace 삭제
    
    * ✨ Dummy 파일 추가
    
    * 📝 Config 파일 수정
    
    * ✨ tuist 버전 차이로 인한 파일 생성
    
    Co-authored-by: wody-d <maxwodyd98@gmail.com>

--- Projects/Marryting/Sources/AppDelegate.swift
+++ Projects/Marryting/Sources/AppDelegate.swift
@@ -1,6 +1,4 @@
 import UIKit
-import MarrytingKit
-import MarrytingUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         viewController.view.backgroundColor = .white
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
-        MarrytingKit.hello()
-        MarrytingUI.hello()
 
         return true
     }
