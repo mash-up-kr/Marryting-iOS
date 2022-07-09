0c131674ecb0561b1f36583729165daee24dbce3
Jaeyong <56102421+wody-d@users.noreply.github.com>
2022-07-09 21:25:55 +0900

    ♻️ Color에 대한 클래스명 수정  (#20)
    * ✨ Colors 추가
    
    * ♻️ Colors 수정
    
    * ♻️ UIColor -> Pallete 클래스명 수정 및 접근지정자 추가

---
+++ Projects/DesignSystem/Sources/Pallete.swift
@@ -0,0 +1,96 @@
+//
+//  Colors+.swift
+//  DesignSystem
+//
+//  Created by Woody on 2022/07/02.
+//  Copyright © 2022 kr.mash-up. All rights reserved.
+//
+
+import UIKit
+
+public enum Pallete {
+    
+    public enum Dark {
+        
+        // MARK: Dark Background Color
+        
+        public static let background: UIColor  = .init(named: "Background_dark")!
+        
+        // MARK: Dark Brand Colors
+        
+        public static let errorRed: UIColor    = .init(named: "Error_Red_dark")!
+        
+        public static let subBlue: UIColor     = .init(named: "Sub_Blue_dark")!
+        
+        public static let subGreen: UIColor    = .init(named: "Sub_Green_dark")!
+        
+        public static let subPurple: UIColor   = .init(named: "Sub_Purple_dark")!
+        
+        public static let subYellow: UIColor   = .init(named: "Sub_Yellow_dark")!
+        
+        // MARK: Dark Greyscale Colors
+        
+        public static let grey100: UIColor  = .init(named: "Grey_100_dark")!
+        
+        public static let grey200: UIColor  = .init(named: "Grey_200_dark")!
+        
+        public static let grey300: UIColor  = .init(named: "Grey_300_dark")!
+        
+        public static let grey400: UIColor  = .init(named: "Grey_400_dark")!
+        
+        public static let grey500: UIColor  = .init(named: "Grey_500_dark")!
+        
+        public static let grey600: UIColor  = .init(named: "Grey_600_dark")!
+        
+        public static let grey700: UIColor  = .init(named: "Grey_700_dark")!
+        
+        public static let grey800: UIColor  = .init(named: "Grey_800_dark")!
+    }
+    
+    public enum Light {
+        
+        // MARK: Light Background Color
+        
+        public static let background: UIColor  = .init(named: "Background_light")!
+        
+        // MARK: Light Brand Colors
+        
+        public static let errorRed: UIColor    = .init(named: "Error_Red_light")!
+        
+        public static let subBlue: UIColor     = .init(named: "Sub_Blue_light")!
+        
+        public static let subGreen: UIColor    = .init(named: "Sub_Green_light")!
+        
+        public static let subPurple: UIColor   = .init(named: "Sub_Purple_light")!
+        
+        public static let subYellow: UIColor   = .init(named: "Sub_Yellow_light")!
+        
+        // MARK: Light Greyscale Colors
+        
+        public static let grey100: UIColor  = .init(named: "Grey_100_light")!
+        
+        public static let grey200: UIColor  = .init(named: "Grey_200_light")!
+        
+        public static let grey300: UIColor  = .init(named: "Grey_300_light")!
+        
+        public static let grey400: UIColor  = .init(named: "Grey_400_light")!
+        
+        public static let grey500: UIColor  = .init(named: "Grey_500_light")!
+        
+        public static let grey600: UIColor  = .init(named: "Grey_600_light")!
+        
+        public static let grey700: UIColor  = .init(named: "Grey_700_light")!
+        
+        public static let grey800: UIColor  = .init(named: "Grey_800_light")!
+    }
+    
+    public static let main100   = UIColor(named: "Main_100")!
+   
+    public static let main200   = UIColor(named: "Main_200")!
+    
+    public static let main300   = UIColor(named: "Main_300")!
+    
+    public static let black    = UIColor(named: "Black")!
+    
+    public static let white    = UIColor(named: "White")!
+}
