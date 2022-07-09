2bf6b3adedb16b2e13da35c2789a60e71b824131
wody-d <maxwodyd98@gmail.com>
2022-07-09 17:28:32 +0900

    ✨ Colors 추가

---
+++ Projects/DesignSystem/Sources/UIColor+.swift
@@ -0,0 +1,40 @@
+//
+//  Colors+.swift
+//  DesignSystem
+//
+//  Created by Woody on 2022/07/02.
+//  Copyright © 2022 kr.mash-up. All rights reserved.
+//
+
+import UIKit
+
+public extension UIColor {
+    
+    // MARK: Background Color
+    
+    static let background = UIColor(named: "Background")!
+    
+    // MARK: Brand Colors
+    
+    static let errorRed  = UIColor(named: "Error_Red")!
+    static let main100   = UIColor(named: "Main_100")!
+    static let main200   = UIColor(named: "Main_200")!
+    static let main300   = UIColor(named: "Main_300")!
+    static let subBlue   = UIColor(named: "Sub_Blue")!
+    static let subGreen  = UIColor(named: "Sub_Green")!
+    static let subPurple = UIColor(named: "Sub_Purple")!
+    static let subYellow = UIColor(named: "Sub_Yellow")!
+    
+    // MARK: Greyscale Colors
+    
+    static let black    = UIColor(named: "Black")!
+    static let grey100  = UIColor(named: "Grey_100")!
+    static let grey200  = UIColor(named: "Grey_200")!
+    static let grey300  = UIColor(named: "Grey_300")!
+    static let grey400  = UIColor(named: "Grey_400")!
+    static let grey500  = UIColor(named: "Grey_500")!
+    static let grey600  = UIColor(named: "Grey_600")!
+    static let grey700  = UIColor(named: "Grey_700")!
+    static let grey800  = UIColor(named: "Grey_800")!
+    static let white    = UIColor(named: "White")!
+}
