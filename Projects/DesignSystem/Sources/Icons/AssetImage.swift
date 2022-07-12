833aa3e38c3700eac8fa36225b33fd777389d1f8
wody-d <maxwodyd98@gmail.com>
2022-07-12 20:29:22 +0900

    ✨ 에셋에 아이콘 추가

--- Projects/DesignSystem/Sources/Icons/AssetImage.swift
+++ Projects/DesignSystem/Sources/Icons/AssetImage.swift
@@ -10,10 +10,22 @@ import UIKit
 
 public enum AssetIcons: String {
     case ic_right_arrow_enable_white
+    case ic_right_arrow_enable_black
     case ic_right_arrow_highlight_green
+    case ic_right_arrow_highlight_red
     case ic_right_arrow_disable
-    case icTrash_enable
-    case icTrash_highlight
+    case ic_trash_enable
+    case ic_trash_highlight
+    case ic_edit
+    case ic_heart_black
+    case ic_heart_red
+    case ic_left_arrow_enable
+    case ic_left_arrow_highlight
+    case ic_clover
+    case ic_check_black
+    case ic_close
+    case ic_dropdown
+    case ic_error
 
     public var image: UIImage? {
         let bundle = Bundle(identifier: "kr.mash-up.DesignSystem")
