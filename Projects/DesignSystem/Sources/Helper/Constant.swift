d08d680e175146429f68ab22e976f53d7536bb1a
wody-d <maxwodyd98@gmail.com>
2022-07-15 18:18:42 +0900

    🔥✨ 버튼 만들 때 도움되는 코드 추가

---
+++ Projects/DesignSystem/Sources/Helper/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  DesignSystem
+//
+//  Created by Woody on 2022/07/15.
+//  Copyright © 2022 kr.mash-up. All rights reserved.
+//
+
+import UIKit
+
+struct Constant {
+    static let circleButtonHeight: CGFloat = 48
+    static let buttonHeight: CGFloat = 56
+    static let longButtonWidth: CGFloat = 240
+}
---
+++ Projects/DesignSystem/Sources/Helper/UIButton+.swift
@@ -0,0 +1,25 @@
+//
+//  UIButton+.swift
+//  DesignSystem
+//
+//  Created by Woody on 2022/07/15.
+//  Copyright © 2022 kr.mash-up. All rights reserved.
+//
+
+import UIKit
+
+public extension UIButton {
+
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        UIGraphicsBeginImageContext(CGSize(width: 1.0, height: 1.0))
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.setFillColor(color.cgColor)
+        context.fill(CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0))
+
+        let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        self.setBackgroundImage(backgroundImage, for: state)
+    }
+    
+}
