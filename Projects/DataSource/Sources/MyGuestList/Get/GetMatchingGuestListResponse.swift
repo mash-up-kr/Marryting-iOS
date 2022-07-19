781f747ba2127bfd4bd9ef1b5730bef7158654e1
wody-d <maxwodyd98@gmail.com>
2022-07-19 20:41:24 +0900

    ✨ Worker 및 Datasource 구현

---
+++ Projects/DataSource/Sources/MyGuestList/Get/GetMatchingGuestListResponse.swift
@@ -0,0 +1,11 @@
+//
+//  GetMatchingGuestListResponse.swift
+//  DataSource
+//
+//  Created by Woody on 2022/07/19.
+//  Copyright © 2022 kr.mash-up. All rights reserved.
+//
+
+import NetworkProtocol
+
+public struct GetMatchingGuestListResponse: Response {}
