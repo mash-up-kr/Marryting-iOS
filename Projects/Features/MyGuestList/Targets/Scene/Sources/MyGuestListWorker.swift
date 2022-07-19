9e0e56244d551cc49d39af2590d611392f3745ea
wody-d <maxwodyd98@gmail.com>
2022-07-20 00:20:24 +0900

    ✨ MatchedGuest 엔티티 추가

--- Projects/Features/MyGuestList/Targets/Scene/Sources/MyGuestListWorker.swift
+++ Projects/Features/MyGuestList/Targets/Scene/Sources/MyGuestListWorker.swift
@@ -15,7 +15,7 @@ import Models
 
 protocol MyGuestListWorkerProtocol {
     func fetchMyLikeGuests() async throws -> [Guest]
-    func fetchMatchingGuests() async throws -> [Guest]
+    func fetchMatchingGuests() async throws -> [MatchedGuest]
 }
 
 final class MyGuestListWorker: MyGuestListWorkerProtocol {
@@ -35,7 +35,7 @@ final class MyGuestListWorker: MyGuestListWorkerProtocol {
         }
     }
 
-    func fetchMatchingGuests() async throws -> [Guest] {
+    func fetchMatchingGuests() async throws -> [MatchedGuest] {
         do {
             let dto = try await myGuestListDataSource.getMatchingGuestList(request: .init())
 
@@ -83,37 +83,43 @@ private extension MyGuestListWorker {
         ]
     }
 
-    var dummyMatchingGuests: [Guest] {
+    var dummyMatchingGuests: [MatchedGuest] {
         [
             .init(
-                user: .init(
-                    id: 3,
-                    name: "박재민",
-                    gender: .male,
-                    career: "IT회사 개발자",
-                    birth: .init(),
-                    age: 25,
-                    address: "비공개",
-                    pictures: ["https://img.theqoo.net/img/fzxPW.jpg"],
-                    answers: [],
-                    keyword: []
+                guest:.init(
+                    user: .init(
+                        id: 3,
+                        name: "박재민",
+                        gender: .male,
+                        career: "IT회사 개발자",
+                        birth: .init(),
+                        age: 25,
+                        address: "비공개",
+                        pictures: ["https://img.theqoo.net/img/fzxPW.jpg"],
+                        answers: [],
+                        keyword: []
+                    ),
+                    isLiked: true
                 ),
-                isLiked: true
+                loveMent: "그대가 찾는 사람, 바로 저입니다"
             ),
             .init(
-                user: .init(
-                    id: 4,
-                    name: "강진호",
-                    gender: .male,
-                    career: "IT회사 개발자",
-                    birth: .init(),
-                    age: 30,
-                    address: "비공개",
-                    pictures: ["https://post-phinf.pstatic.net/MjAxOTAyMTZfMTMz/MDAxNTUwMjg0NTQ3Njk3.nOALV-TOkthnpIEg3kFCA6QA221DrLgZsBJxMKE1Hj0g.I_EYQvpkiwhl8Jj9sTBfNIs5U7Hai968vAqa5BJHlpAg.JPEG/n1233.jpg?type=w1200"],
-                    answers: [],
-                    keyword: []
+                guest: .init(
+                    user: .init(
+                        id: 4,
+                        name: "강진호",
+                        gender: .male,
+                        career: "IT회사 개발자",
+                        birth: .init(),
+                        age: 30,
+                        address: "비공개",
+                        pictures: ["https://post-phinf.pstatic.net/MjAxOTAyMTZfMTMz/MDAxNTUwMjg0NTQ3Njk3.nOALV-TOkthnpIEg3kFCA6QA221DrLgZsBJxMKE1Hj0g.I_EYQvpkiwhl8Jj9sTBfNIs5U7Hai968vAqa5BJHlpAg.JPEG/n1233.jpg?type=w1200"],
+                        answers: [],
+                        keyword: []
+                    ),
+                    isLiked: false
                 ),
-                isLiked: false
+                loveMent: "그대의 눈동자에 cheers~!"
             )
         ]
     }
